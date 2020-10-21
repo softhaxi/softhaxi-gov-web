e764ee1d939b350568a36aab0d1a5bfc996e7200
Raja Sihombing <if09051@gmail.com>
2020-10-21 08:01:50 +0800

    Implement LDAP authentication

---
+++ src/main/java/com/softhaxi/marves/core/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.softhaxi.marves.core.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * @author Raja Sihombing
+ * @since 1
+ */
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().anyRequest()
+			.fullyAuthenticated().and().formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+			.ldapAuthentication()
+				.userDnPatterns("uid={0},ou=people")
+				.groupSearchBase("ou=groups")
+				.contextSource()
+					.url("ldap://localhost:8389/dc=springframework,dc=org")
+					.and()
+				.passwordCompare()
+					.passwordEncoder(new BCryptPasswordEncoder())
+					.passwordAttribute("userPassword");
+    }
+
+    
+    
+}

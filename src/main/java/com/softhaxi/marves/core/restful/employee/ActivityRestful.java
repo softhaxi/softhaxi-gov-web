7524bcf1a0917ecf71ed4a3e025962efe7172cc8
Raja Sihombing <if09051@gmail.com>
2020-10-27 16:09:30 +0800

    Update Activity API List Service

--- src/main/java/com/softhaxi/marves/core/restful/employee/ActivityRestful.java
+++ src/main/java/com/softhaxi/marves/core/restful/employee/ActivityRestful.java
@@ -1,17 +1,15 @@
 package com.softhaxi.marves.core.restful.employee;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 import com.softhaxi.marves.core.domain.account.User;
-import com.softhaxi.marves.core.domain.logging.ActivityLog;
 import com.softhaxi.marves.core.model.response.GeneralResponse;
 import com.softhaxi.marves.core.repository.logging.ActivityLogRepository;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -28,26 +26,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/activity")
 public class ActivityRestful {
-    private static final Logger logger = LoggerFactory.getLogger(ActivityRestful.class);
+    // private static final Logger logger =
+    // LoggerFactory.getLogger(ActivityRestful.class);
 
     @Autowired
     private ActivityLogRepository activityLogRepository;
 
     @GetMapping()
     public ResponseEntity<?> index(
-        @RequestParam(value = "range", defaultValue = "today") String range) {
-        List<ActivityLog> activities = null;
-
+            @RequestParam(name = "date", required = false) @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         User user = new User().id(UUID.fromString(auth.getPrincipal().toString()));
 
-        if(range.equals("today")) {
-            logger.debug("[ActivityRestful][index] List of today activity...");
-            activities = activityLogRepository.findByUserAndActionDateOrderByActionTimeDesc(user, LocalDate.now());
-        }
+        if (date == null)
+            date = LocalDate.now();
 
-        return new ResponseEntity<>(
-            new GeneralResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), activities), 
-            HttpStatus.OK);
+        return new ResponseEntity<>(new GeneralResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(),
+                activityLogRepository.findByUserAndActionDateOrderByActionTimeDesc(user, date)), HttpStatus.OK);
     }
 }

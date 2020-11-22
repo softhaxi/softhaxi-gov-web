466cdb802f4d499bbb447fc302fe4c7ea17070d8
Raja Sihombing <if09051@gmail.com>
2020-11-22 23:27:26 +0800

    Add mobile asset

--- src/main/java/com/softhaxi/marves/core/configuration/WebAssetConfiguration.java
+++ src/main/java/com/softhaxi/marves/core/configuration/WebAssetConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 @Configuration
 public class WebAssetConfiguration implements WebMvcConfigurer {
 
+    @Value("${app.mobile.asset}")
+    private String mobileAsset;
+
     @Value("${app.upload.path}")
     private String uploadPath;
 
@@ -16,8 +19,11 @@ public class WebAssetConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         if(!uploadPath.endsWith("/"))
             uploadPath += "/";
+        if(!mobileAsset.endsWith("/"))
+            mobileAsset += "/";
         registry.addResourceHandler("/asset/**")
-            .addResourceLocations(String.format("file:%s", uploadPath))
+            .addResourceLocations(String.format("file:%s", uploadPath), 
+                String.format("file:%s", mobileAsset))
             .setCachePeriod(3600)
             .resourceChain(true)
             .addResolver(new PathResourceResolver());

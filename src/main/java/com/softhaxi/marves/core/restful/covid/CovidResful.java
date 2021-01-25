6e6d5ce088b27bc7d214d83530fd772feef7ac68
Softh Axi <softhaxi@gmail.com>
2021-01-25 19:19:26 +0700

    Update logger

--- src/main/java/com/softhaxi/marves/core/restful/covid/CovidResful.java
+++ src/main/java/com/softhaxi/marves/core/restful/covid/CovidResful.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import com.softhaxi.marves.core.domain.master.SystemParameter;
 import com.softhaxi.marves.core.repository.master.SystemParameterRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/api/v1/covid")
 public class CovidResful {
 
+    private static final Logger logger = LoggerFactory.getLogger(CovidResful.class);
+
     @Autowired
     private SystemParameterRepository sysParamRepo;
 
@@ -24,6 +28,7 @@ public class CovidResful {
         RestTemplate restTemplate = new RestTemplate();
 
         SystemParameter param = sysParamRepo.findByCode("COVIDTRACKER_API_URL").orElse(null);
+        // logger.info(param.toString());
         if(param != null) {
             return restTemplate.getForEntity(param.getValue() + "/statistic", Map.class);
         }

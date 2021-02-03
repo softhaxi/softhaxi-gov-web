0565353bc9f548a00f071db533da35a25197907d
Softh Axi <softhaxi@gmail.com>
2021-02-03 11:52:00 +0700

    Update history of attendance service

--- src/main/java/com/softhaxi/marves/core/restful/employement/AbsenceRestful.java
+++ src/main/java/com/softhaxi/marves/core/restful/employement/AbsenceRestful.java
@@ -2,14 +2,22 @@ package com.softhaxi.marves.core.restful.employement;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -18,10 +26,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.softhaxi.marves.core.domain.account.User;
 import com.softhaxi.marves.core.domain.attendance.Attendance;
 import com.softhaxi.marves.core.domain.attendance.DailyAttendance;
+import com.softhaxi.marves.core.domain.attendance.Dispensation;
 import com.softhaxi.marves.core.domain.attendance.MeetingAttendance;
 import com.softhaxi.marves.core.model.request.AbsenceRequest;
 import com.softhaxi.marves.core.model.response.ErrorResponse;
 import com.softhaxi.marves.core.model.response.GeneralResponse;
+import com.softhaxi.marves.core.repository.attendance.DailyAttendanceRepository;
 import com.softhaxi.marves.core.repository.attendance.DispensationRepository;
 import com.softhaxi.marves.core.service.employee.AbsenceService;
 import com.softhaxi.marves.core.service.storage.FileStorageService;
@@ -58,6 +68,9 @@ public class AbsenceRestful {
     private AbsenceService absenceService;
 
     @Autowired
+    private DailyAttendanceRepository dailyRepo;
+
+    @Autowired
     private DispensationRepository dispensationRepo;
 
     @Autowired
@@ -80,15 +93,60 @@ public class AbsenceRestful {
             from = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), 1);
 
         LocalDate to = null;
+        if(now.getYear() == from.getYear() && now.getMonthValue() < from.getMonthValue()) {
+            return new ResponseEntity<>(
+                new GeneralResponse(
+                    HttpStatus.OK.value(),
+                    HttpStatus.OK.getReasonPhrase(),
+                    null
+                    ),
+                HttpStatus.OK
+            );
+        }
+
         if(now.getYear() == from.getYear() && now.getMonthValue() == from.getMonthValue()) {
             to = now;
         } else {
-            to = from.with(TemporalAdjusters.lastDayOfMonth());
+            to = from.with(TemporalAdjusters.lastDayOfMonth()).plusDays(1);
         }
-        logger.info("[History] From date..." + from.toString() + " To date..." + to.toString());
+        final long days = from.until(to, ChronoUnit.DAYS);
+        List<LocalDate> dateRange = Stream.iterate(from, 
+            d -> d.plusDays(1))
+            .limit(days)
+            .collect(Collectors.toList());
+        logger.info("[History] From date..." + from.toString() 
+            + " To date..." + to.toString() + " Number of dates..." + dateRange.size());
 
-        Collection<?> data = absenceService.getHistoryByUser(user, type.trim().toLowerCase(), from.atStartOfDay(ZoneId.systemDefault()), 
+        Collection<?> absences = absenceService.getHistoryByUser(user, type.trim().toLowerCase(), from.atStartOfDay(ZoneId.systemDefault()), 
             to.atStartOfDay(ZoneId.systemDefault()));
+        Collection<?> dispensations = dispensationRepo.findByUserAndBetweenDates(user, from, to);
+        logger.info("[History] Dispensation number..." + dispensations.size());
+
+        List<Attendance> data = new LinkedList<>();
+        dateRange.forEach((date) -> {
+            Attendance attendance = (Attendance) absences.stream()
+                .filter((item) -> ((Attendance)item).getDateTime().toLocalDate().equals(date))
+                .findFirst().orElse(null);
+            if(attendance == null) {
+                attendance = new DailyAttendance();
+                attendance.setDateTime(date.atStartOfDay(ZoneId.systemDefault()).plusHours(1));
+                var dispensation = dispensations.stream()
+                    .filter((item) -> ((Dispensation)item).getStartDate().equals(date) ||
+                    (((Dispensation)item).getStartDate().isBefore(date) && ((Dispensation)item).getEndDate().isAfter(date)) ||
+                    ((Dispensation)item).getEndDate().equals(date)).findFirst().orElse(null);
+                if(dispensation != null) {
+                    // logger.info("[History] Dispensation..." +dispensation + " Date..." + date.toString());
+                    ((DailyAttendance) attendance).setDispensation((Dispensation) dispensation);
+                }
+            } 
+            data.add(attendance);
+        });
+        Collections.sort(data, new Comparator<Attendance>() {
+            @Override
+            public int compare(Attendance o1, Attendance o2) {
+                return o2.getDateTime().compareTo(o1.getDateTime());
+            }
+        });
 
         return new ResponseEntity<>(
             new GeneralResponse(
@@ -142,22 +200,24 @@ public class AbsenceRestful {
                 HttpStatus.BAD_REQUEST
             );
         }
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = new User().id(UUID.fromString(auth.getPrincipal().toString()));
+        
         String ipAddress = servlet.getHeader("X-Forwarded-For") != null ? servlet.getHeader("X-Forwarded-For") : servlet.getRemoteAddr();
         //logger.info("[post] IP Adress..." + ipAddress);
 
         String path = null;
         if (file != null) {
             try {
+                String folder = String.format("/%s/%s", request.getType().toLowerCase(), user.getId());
+                String filename = String.format("%s_%s", new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), request.getAction().toLowerCase());
                 //String folder = String.format("/%s/%s", request.getType().toLowerCase(), new SimpleDateFormat("yyyyMMdd").format(new Date()));
-                path = storageService.store(String.format("/%s",request.getType().toLowerCase()), new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()), file);
+                path = storageService.store(folder, filename, file);
             } catch (IOException e) {
                 logger.error(e.getMessage(), e);
             }
         }
 
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = new User().id(UUID.fromString(auth.getPrincipal().toString()));
         Attendance attendence = absenceService.getLastAbsence(user, request.getType(), request.getCode());
         if(attendence.getId() != null) {
             if(attendence instanceof DailyAttendance) {
@@ -209,7 +269,7 @@ public class AbsenceRestful {
                     daily.setOutLongitude(request.getLongitude());
                     daily.setIsOutMockLocation(request.getIsMockLocation());
                     if(path != null) {
-                        daily.setPicturePath(path);
+                        daily.setOutPicturePath(path);
                     }
                     daily.setOutIpAddress(ipAddress);
                     attendence = absenceService.save(daily);
@@ -358,6 +418,25 @@ public class AbsenceRestful {
                 HttpStatus.OK.value(),
                 HttpStatus.OK.getReasonPhrase(),
                 null
+            ),
+            HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/history/year")
+    public ResponseEntity<?> year(@RequestParam(required = false, defaultValue = "daily") String type) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = new User().id(UUID.fromString(auth.getPrincipal().toString()));
+        Collection<Object[]> years = dailyRepo.findAllYearsByUser(user);
+
+        List<String> data = new LinkedList<>();
+        years.stream().forEach((item) -> data.add(item[0].toString()));
+
+        return new ResponseEntity<>(
+            new GeneralResponse(
+                HttpStatus.OK.value(),
+                HttpStatus.OK.getReasonPhrase(),
+                data
             ),
             HttpStatus.OK
         );

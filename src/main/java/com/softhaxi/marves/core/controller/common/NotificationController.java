bbad31f1262d23f279f763dd936f97dc8440fe3a
Andryan Situngkir <and.dryan@gmail.com>
2021-02-07 21:05:43 +0800

    Notification Menu
    Enable Push Notification from web to user mobile device

--- src/main/java/com/softhaxi/marves/core/controller/common/NotificationController.java
+++ src/main/java/com/softhaxi/marves/core/controller/common/NotificationController.java
@@ -1,12 +1,18 @@
 package com.softhaxi.marves.core.controller.common;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.softhaxi.marves.core.domain.account.User;
 import com.softhaxi.marves.core.domain.messaging.Message;
@@ -14,9 +20,15 @@ import com.softhaxi.marves.core.domain.messaging.Notification;
 import com.softhaxi.marves.core.repository.account.UserRepository;
 import com.softhaxi.marves.core.repository.messaging.NotificationRepository;
 
+import org.apache.groovy.util.Maps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +36,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Notification
@@ -39,6 +52,16 @@ public class NotificationController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    @Qualifier("oneSignalRestTemplate")
+    private RestTemplate restTemplate;
+
+    @Value("${onesignal.app.id}")
+    private String appId;
+
+    @Value("${onesignal.notification.endpoint}")
+    private String notificationEndPoint;
+
     @GetMapping("/notification")
     public String getAllNotification(Model model){
         List<Notification> notifications = notificationRepository.findAll();
@@ -49,16 +72,53 @@ public class NotificationController {
 
     @PostMapping("/save-notification")
     public String saveNotification(Model model, @ModelAttribute("notification") Notification notification){
+        String subscriber = "";
+        logger.debug("Assignee: " + notification.getAssignee());
+        if(null==notification.getAssignee() || notification.getAssignee().equals("")){
+            subscriber = "Subscribed Users";
+        }else{
+            Optional<User> user = userRepository.findUserByEmail(notification.getAssignee());
+            if(user.isPresent()){
+                notification.setUser(user.get());
+                notification.setDateTime(ZonedDateTime.now(ZoneId.systemDefault()));
+                notificationRepository.save(notification);    
+            }
+            else{
+                model.addAttribute("errorMessage", "Email "+notification.getAssignee()+" is not attached to any user");
+            }
+        }
         
-        Optional<User> user = userRepository.findUserByEmail(notification.getAssignee());
-        if(user.isPresent()){
-            notification.setUser(user.get());
-            notificationRepository.save(notification);    
+       
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.add("Content-Type", "application/json; charset=utf-8");
+        
+        Map<?, ?> body = new HashMap<>();
+        if(!subscriber.equals("")){
+            body = Maps.of(
+                "app_id", appId,
+                "included_segments", Arrays.asList(subscriber),
+                "headings", ImmutableMap.of("en", notification.getCategory()),
+                "contents", ImmutableMap.of("en", notification.getContent()),
+                "template_id", "8d98a080-04d2-4873-bf29-3c463ce6866a"
+            );
+        }else{
+            body = Maps.of(
+                "app_id", appId,
+                "include_player_ids", Arrays.asList(notification.getUser().getOneSignalId()),
+                "headings", ImmutableMap.of("en", notification.getCategory()),
+                "contents", ImmutableMap.of("en", notification.getContent()),
+                "template_id", "8d98a080-04d2-4873-bf29-3c463ce6866a"
+            );
         }
-        else{
-            model.addAttribute("errorMessage", "Email "+notification.getAssignee()+" is not attached to any user");
-        }
+        
+        HttpEntity<Map<?, ?>> entity = new HttpEntity<>(body, headers);
+        ResponseEntity<?> response = restTemplate.postForEntity(notificationEndPoint, entity, Map.class);
+    
+        logger.info("[sendNotification] Result....{}", response.getBody());
+
         List<Notification> notifications = notificationRepository.findAll();
+        
         model.addAttribute("notifications", notifications);
         return "common/notification-list.html";
     }

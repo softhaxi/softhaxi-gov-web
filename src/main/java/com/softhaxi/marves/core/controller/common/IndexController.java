b853ada5e2bea8900fd6dca26f4ec027c4226f75
Softh Axi <softhaxi@gmail.com>
2021-02-13 12:38:31 +0700

    Redesign Dashboard menu

--- src/main/java/com/softhaxi/marves/core/controller/common/IndexController.java
+++ src/main/java/com/softhaxi/marves/core/controller/common/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -55,7 +57,7 @@ public class IndexController {
 		return "common/index";
 	}
 
-	@GetMapping("/dashboard")
+	// @GetMapping("/dashboard")
 	public String dashboard(Model model) {
 		model.addAttribute("latestUpdated", 
 			activityLogRepo.findAll(PageRequest.of(0, 5, 

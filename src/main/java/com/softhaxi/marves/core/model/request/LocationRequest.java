3af086480cc7b10811933bee6dda6057566ee42a
Softh Axi <softhaxi@gmail.com>
2021-02-16 18:42:19 +0700

    Add location service

--- src/main/java/com/softhaxi/marves/core/model/request/LocationRequest.java
+++ src/main/java/com/softhaxi/marves/core/model/request/LocationRequest.java
@@ -1,5 +1,85 @@
 package com.softhaxi.marves.core.model.request;
 
+import java.util.Date;
+
 public class LocationRequest {
     
+    private Date dateTime;
+    private double latitude;
+    private double longitude;
+    private boolean isMockLocation;
+
+
+    public LocationRequest() {
+    }
+
+    public LocationRequest(Date dateTime, double latitude, double longitude, boolean isMockLocation) {
+        this.dateTime = dateTime;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.isMockLocation = isMockLocation;
+    }
+
+    public Date getDateTime() {
+        return this.dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public boolean isMockLocation() {
+        return this.isMockLocation;
+    }
+
+    public void setIsMockLocation(boolean isMockLocation) {
+        this.isMockLocation = isMockLocation;
+    }
+
+    public LocationRequest dateTime(Date dateTime) {
+        setDateTime(dateTime);
+        return this;
+    }
+
+    public LocationRequest latitude(double latitude) {
+        setLatitude(latitude);
+        return this;
+    }
+
+    public LocationRequest longitude(double longitude) {
+        setLongitude(longitude);
+        return this;
+    }
+
+    public LocationRequest isMockLocation(boolean isMockLocation) {
+        setIsMockLocation(isMockLocation);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " dateTime='" + getDateTime() + "'" +
+            ", latitude='" + getLatitude() + "'" +
+            ", longitude='" + getLongitude() + "'" +
+            ", isMockLocation='" + isMockLocation() + "'" +
+            "}";
+    }
+
 }
---
+++ src/main/java/com/softhaxi/marves/core/restful/support/LocationResful.java
@@ -0,0 +1,72 @@
+package com.softhaxi.marves.core.restful.support;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.softhaxi.marves.core.domain.account.User;
+import com.softhaxi.marves.core.domain.logging.LocationLog;
+import com.softhaxi.marves.core.model.request.LocationRequest;
+import com.softhaxi.marves.core.model.response.ErrorResponse;
+import com.softhaxi.marves.core.model.response.GeneralResponse;
+import com.softhaxi.marves.core.repository.logging.LocationLogRepository;
+import com.softhaxi.marves.core.service.logging.LoggerService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/location")
+public class LocationResful {
+
+    @Autowired
+    private LoggerService loggerService;
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody LocationRequest request,
+        HttpServletRequest servlet) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = new User().id(UUID.fromString(auth.getPrincipal().toString()));
+
+        String ipAddress = servlet.getHeader("X-Forwarded-For") != null ? servlet.getHeader("X-Forwarded-For") : servlet.getRemoteAddr();
+        
+        try {
+            LocationLog location = new LocationLog()
+                .user(user)
+                .dateTime(ZonedDateTime.ofInstant(request.getDateTime().toInstant(), ZoneId.systemDefault()))
+                .latitude(request.getLatitude())
+                .longitude(request.getLongitude())
+                .isMockLocation(request.isMockLocation());
+            // locationRepo.save(location);
+            loggerService.saveAsyncLocationLog(location);
+
+            return new ResponseEntity<>(
+                new GeneralResponse(
+                    HttpStatus.CREATED.value(), 
+                    HttpStatus.CREATED.getReasonPhrase(), 
+                    location
+                ), 
+                HttpStatus.CREATED);
+        } catch(Exception ex) {
+            return new ResponseEntity<>(
+                new ErrorResponse(
+                    HttpStatus.BAD_REQUEST.value(), 
+                    HttpStatus.BAD_REQUEST.getReasonPhrase(), 
+                    ex.getMessage()
+                ), 
+                HttpStatus.BAD_REQUEST);
+        } finally {
+
+        }
+        
+    }
+}

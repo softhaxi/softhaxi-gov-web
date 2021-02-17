50503359e380bf430178ce17d2f4eb9ecc6ce8f7
Softh Axi <softhaxi@gmail.com>
2021-02-17 19:28:55 +0700

    Add datepicker config

---
+++ src/main/resources/static/scripts/app.js
@@ -0,0 +1,12 @@
+$.fn.datepicker.dates['id'] = {
+    days: ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"],
+    daysShort: ["Min", "Sen", "Sel", "Rab", "Kam", "Jum", "Sab"],
+    daysMin: ["Mi", "Se", "Se", "Ra", "Ka", "Ju", "Sa"],
+    months: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "Desember"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
+    today: "Hari ini",
+    clear: "Hapus",
+    format: "dd MM yyyy",
+    titleFormat: "MM yyyy", /* Leverages same syntax as 'format' */
+    weekStart: 0
+};

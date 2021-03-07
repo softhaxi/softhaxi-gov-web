22cd3267886dd86afc9eccb295c226d3d7fea0e1
Softh Axi <softhaxi@gmail.com>
2021-03-07 13:04:10 +0700

    Update UserRequest.java

--- src/main/java/com/softhaxi/marves/core/model/request/UserRequest.java
+++ src/main/java/com/softhaxi/marves/core/model/request/UserRequest.java
@@ -7,34 +7,63 @@ import java.io.Serializable;
  */
 public class UserRequest implements Serializable {
 
+    private String action;
+    private String id;
+    private String roleId;
     private String oneSignalId;
 
-    @Override
-    public boolean equals(Object obj) {
-        // TODO Auto-generated method stub
-        return super.equals(obj);
+
+    public UserRequest() {
     }
 
-    @Override
-    public int hashCode() {
-        // TODO Auto-generated method stub
-        return super.hashCode();
+    public UserRequest(String action, String id, String roleId, String oneSignalId) {
+        this.action = action;
+        this.id = id;
+        this.roleId = roleId;
+        this.oneSignalId = oneSignalId;
     }
 
-    @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+    public String getAction() {
+        return this.action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleId() {
+        return this.roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
     }
 
     public String getOneSignalId() {
-        return oneSignalId;
+        return this.oneSignalId;
     }
 
     public void setOneSignalId(String oneSignalId) {
         this.oneSignalId = oneSignalId;
     }
 
+    @Override
+    public String toString() {
+        return "{" +
+            " action='" + getAction() + "'" +
+            ", id='" + getId() + "'" +
+            ", roleId='" + getRoleId() + "'" +
+            ", oneSignalId='" + getOneSignalId() + "'" +
+            "}";
+    }
     
     
 }

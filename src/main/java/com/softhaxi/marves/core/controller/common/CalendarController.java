9f947a0f0a01463ba72fbea95f09fb750e63cbb9
Softh Axi <softhaxi@gmail.com>
2021-03-07 12:55:18 +0700

    Update calendar event

--- src/main/java/com/softhaxi/marves/core/controller/common/CalendarController.java
+++ src/main/java/com/softhaxi/marves/core/controller/common/CalendarController.java
@@ -9,19 +9,21 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.UUID;
 
-import com.google.gson.Gson;
 import com.softhaxi.marves.core.domain.master.CalendarEvent;
+import com.softhaxi.marves.core.model.request.CalendarEventRequest;
+import com.softhaxi.marves.core.model.response.ErrorResponse;
+import com.softhaxi.marves.core.model.response.GeneralResponse;
 import com.softhaxi.marves.core.repository.master.CalendarEventRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/calendar")
@@ -36,49 +38,46 @@ public class CalendarController {
     }
 
     @PostMapping("/event")
-    public @ResponseBody String postEvent(@RequestParam(name = "name") String name,
-            @RequestParam(name = "date") @DateTimeFormat(iso = ISO.DATE) LocalDate date) {
-        CalendarEvent event = new CalendarEvent().date(date).name(name.trim());
-        calendarEventRepo.save(event);
-
+    public ResponseEntity<?> actionEvent(@RequestBody CalendarEventRequest request) {
         Map<String, Object> map = new HashMap<>();
-        map.put("id", event.getId());
-        map.put("action", "inserted");
-
-        Gson gson = new Gson();
-        return gson.toJson(map);
-    }
-
-    @PostMapping("/event/action")
-    public @ResponseBody String actionEvent(
-        @RequestParam(name = "id") String id,
-        @RequestParam(name = "action") String action) {
-        Map<String, Object> map = new HashMap<>();
-        Gson gson = new Gson();
-    
-        UUID uuid;
-        try {
-            uuid = UUID.fromString(id);
-        } catch(IllegalArgumentException ex) {
-            map.put("id", id);
-            map.put("action", "deleted");
-            return gson.toJson(map);
-        }
-        CalendarEvent event = calendarEventRepo.findById(uuid).orElseThrow();
-        
-        map.put("id", event.getId());
-        if(action.equalsIgnoreCase("delete")) {
+        CalendarEvent event = null;
+        if(request.getAction().equalsIgnoreCase("insert"))
+            event = new CalendarEvent().date(request.getDate()).name(request.getName().trim());
+        else if(request.getAction().equalsIgnoreCase("delete")) {
+            UUID uuid;
+            try {
+                uuid = UUID.fromString(request.getId());
+            } catch (IllegalArgumentException ex) {
+                map.put("id", request.getId());
+                map.put("action", "deleted");
+                return new ResponseEntity<>(
+                        new ErrorResponse(
+                            HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), map), 
+                            HttpStatus.NOT_FOUND);
+            }
+            event = calendarEventRepo.findById(uuid).orElse(null);
+            if(event == null) {
+                return new ResponseEntity<>(
+                        new ErrorResponse(
+                            HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND.getReasonPhrase(), "item.not.found"), 
+                            HttpStatus.NOT_FOUND);
+            }
             event.setDeleted(true);
-            map.put("action", "deleted");
-        } 
+        }
         calendarEventRepo.save(event);
 
-        
-        return gson.toJson(map);
+        map.put("id", event.getId());
+        if(request.getAction().equalsIgnoreCase("insert"))
+            map.put("action", "inserted");
+        else if(request.getAction().equalsIgnoreCase("delete"))
+            map.put("action", "deleted");
+
+        return new ResponseEntity<>(new GeneralResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), map),
+                HttpStatus.OK);
     }
 
     @GetMapping("/event/search")
-    public @ResponseBody String searchEvents(@RequestParam(name = "year", required = false) String paramYear) {
+    public ResponseEntity<?> searchEvents(@RequestParam(name = "year", required = false) String paramYear) {
         int year = LocalDate.now().getYear();
         if (paramYear != null)
             year = Integer.parseInt(paramYear);
@@ -96,7 +95,7 @@ public class CalendarController {
             map.put("end_date", event.getDate().plusDays(1).atStartOfDay(ZoneId.systemDefault()).format(formatter));
             data.add(map);
         }
-        Gson gson = new Gson();
-        return gson.toJson(data);
+        return new ResponseEntity<>(new GeneralResponse(HttpStatus.OK.value(), HttpStatus.OK.getReasonPhrase(), data),
+                HttpStatus.OK);
     }
 }
